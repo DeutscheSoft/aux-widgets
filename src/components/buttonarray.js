90c01f0fd5b0ec4ecb9f2045a7292d0237920f47
Markus Schmidt <schmidt@boomshop.net>
2019-10-22 17:43:48 +0200

    Remove ButtonArray pt. 2

--- src/components/buttonarray.js
+++
@@ -1,8 +0,0 @@
-import {
-    component_from_widget, define_component
-  } from './../component_helpers.js';
-import { ButtonArray } from './../widgets/buttonarray.js';
-
-export const ButtonArrayComponent = component_from_widget(ButtonArray);
-
-define_component('buttonarray', ButtonArrayComponent);
