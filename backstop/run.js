b4f184d61693646fd6eca29e76379923806dabf1
Arne Goedeke <arne@deuso.de>
2019-10-27 11:51:00 +0100

    [backstop]: do not open browser after running test

--- backstop/run.js
+++ backstop/run.js
@@ -27,7 +27,7 @@ const config = {
     "html_report": "backstop_data/html_report",
     "ci_report": "backstop_data/ci_report"
   },
-  "report": ["browser"],
+  "report": [],
   "engine": "puppeteer",
   "engineOptions": {
     "args": ["--no-sandbox"],
