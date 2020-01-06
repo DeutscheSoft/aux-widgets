2119a529e3d5321d043522f3816373f6ea108164
Markus Schmidt <schmidt@boomshop.net>
2020-01-06 22:45:04 +0100

    Add more information about disabled option; move style of .aux-disabled to aux.css

--- src/widgets/widget.js
+++ src/widgets/widget.js
@@ -149,7 +149,7 @@ export const Widget = define_class({
      * @property {HTMLElement} [options.container] - A container the main element shall be added to.
      * @property {String} [options.id=""] - A string to be set as id attribute on the main element.
      * @property {Object} [options.styles=""] - An object containing CSS declarations to be added directly to the main element.
-     * @property {Boolean} [options.disabled=false] - Toggles the class <code>.aux-disabled</code>.
+     * @property {Boolean} [options.disabled=false] - Toggles the class <code>.aux-disabled</code>. By default it disables all pointer events on the widget via CSS to make it unusable to the user.
      * @property {HTMLElement} [options.element] - An element to be used as the main element.
      * @property {Boolean} [options.active] - Toggles the class <code>.aux-inactive</code>.
      * @property {Boolean} [options.needs_resize=true] - Set to true if the resize function shall be called before the next redraw.
