a187f246ed09d646faecf7bb8175e48d211f9310
Markus Schmidt <schmidt@boomshop.net>
2020-04-16 02:25:58 +0200

    Firs steps on Entry widget

--- src/matrix/widgets/entry.js
+++ src/matrix/widgets/entry.js
@@ -1,17 +1,52 @@
-import { Widget } from './widget.js';
+import { define_class } from '../../widget_helpers.js';
+import { define_child_widget } from '../../child_widget.js';
+import { Container } from '../../widgets/container.js';
 import { Label } from '../../widgets/label.js';
 
 export const Entry = define_class({
-    Extends: Widget,
-    _options: Object.assign(Object.create(Widget.prototype._options), {
+    Extends: Container,
+    _options: Object.assign(Object.create(Container.prototype._options), {
         label: "string|boolean",
-        depth: "number",
+        depth: "number|boolean",
+        last: "boolean",
     }),
+    options: {
+        label: false,
+        depth: false,
+        last: false,
+    },
     initialize: function (options) {
         if (!options.element) options.element = element('div');
-        Widget.prototype.initialize.call(this, options);
+        Container.prototype.initialize.call(this, options);
         this.datum = null;
         this.datumSubscription = null;
+    },
+    draw: function (options, element) {
+        Container.prototype.draw.call(this, options, element);
+        element.classList.add("aux-entry");
+    },
+    redraw: function () {
+        var O = this.options;
+        var E = this.element;
+        var I = this.invalid;
+        
+        if (I.depth) {
+            I.depth = false;
+            var C = E.getAttribute("class");
+            C = C.replace(/aux-depth-[0-9]*/mg, "");
+            C = C.replace(/\s\s+/g, ' ');
+            E.setAttribute("class", C);
+            E.classList.add("aux-depth-" + O.depth);
+            E.style.setProperty('--aux-entry-depth', O.depth)
+        }
+        
+        if (I.last) {
+            I.last = false;
+            E.classList[O.last ? "add" : "remove"]("aux-last");
+        }
+        
+        Container.prototype.redraw.call(this);
+        
     },
     setDatum: function(datum) {
         if (this.datumSubscription)
@@ -58,4 +93,13 @@ define_child_widget(Entry, "label", {
     option: "label",
     inherit_options: true,
     toggle_class: true,
+});
+
+define_child_widget(Entry, "indent", {
+    create: Container,
+    option: "depth",
+    toggle_class: true,
+    default_options: {
+        class: "aux-indent",
+    },
 });
