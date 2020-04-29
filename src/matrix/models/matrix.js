47d2e5e6463b331d8d2f4b8410a3d76b6b7bcf2f
Arne Goedeke <arne@deuso.de>
2020-04-29 17:18:50 +0200

    Matrix: added basic APIs for managing connections

--- src/matrix/models/matrix.js
+++ src/matrix/models/matrix.js
@@ -3,6 +3,7 @@ import { PortData } from './port.js';
 import { GroupData } from './group.js';
 import { ListDataView } from './listdataview.js';
 import { TreeNodeData } from './treenode.js';
+import { ConnectionData } from './connection.js';
 
 export class MatrixData extends Datum
 {
@@ -12,6 +13,9 @@ export class MatrixData extends Datum
     this.root = this.createGroup();
     this.nodes = new Map();
     this.last_id = 0;
+
+    // Map<PortData, Map<PortData, ConnectionData>>
+    this.connections = new Map();
   }
 
   registerNode(node)
@@ -39,6 +43,10 @@ export class MatrixData extends Datum
 
     if (nodes.get(id) !== node)
       throw new Error('Removing unknown node.');
+
+    this.getConnectionsOf(node).forEach((connection) => {
+      this.deleteConnection(connection);
+    });
 
     nodes.delete(id);
   }
@@ -115,12 +123,112 @@ export class MatrixData extends Datum
 
   // APIs for managing connections
 
+
+  _lowRegisterConnection(from, to, connection)
+  {
+    let connections_from = this.connections.get(from);
+
+    if (!connections_from)
+    {
+      connections_from = new Map();
+      this.connections.set(from, connections_from);
+    }
+
+    if (connections_from.has(to))
+    {
+      throw new Error('Connection already exists.');
+    }
+
+    let connections_from_to = connections_from.get(to);
+
+    connections_from.set(to, connection);
+  }
+
+  _lowUnregisterConnection(from, to, connection)
+  {
+    let connections_from = this.connections.get(from);
+
+    if (connections_from)
+    {
+      const con = connections_from.get(to);
+
+      if (con === connection)
+      {
+        connections_from.delete(to);
+
+        if (!connections_from.size)
+        {
+          this.connections.delete(from);
+        }
+
+        return;
+      }
+    }
+
+    throw new Error('Could not find connection.');
+  }
+
+  _registerConnection(connection)
+  {
+    this._lowRegisterConnection(connection.from, connection.to, connection);
+    this._lowRegisterConnection(connection.to, connection.from, connection);
+    this.emit('connectionAdded', connection);
+  }
+
+  _unregisterConnection(connection)
+  {
+    this._lowUnregisterConnection(connection.from, connection.to, connection);
+    this._lowUnregisterConnection(connection.to, connection.from, connection);
+    this.emit('connectionRemoved', connection);
+  }
+
+  createConnection(connection)
+  {
+    if (!(connection instanceof ConnectionData))
+    {
+      connection = new ConnectionData(this, connection);
+    }
+
+    return connection;
+  }
+
   addConnection(connection)
   {
+    connection = this.createConnection(connection);
+
+    this._registerConnection(connection);
+
+    return connection;
   }
 
   deleteConnection(connection)
   {
+    this._unregisterConnection(connection);
+  }
+
+  connect(from, to)
+  {
+    return this.addConnection({
+      from: from,
+      to: to,
+    });
+  }
+
+  getConnectionsOf(node)
+  {
+    const connections_map = this.connections.get(node);
+
+    return connections_map ? Array.from(connections_map.values()) : [];
+  }
+
+  getConnectionsFrom(node)
+  {
+    return this.getConnectionsOf(node).filter((connection) => connection.from === node);
+  }
+
+  getConnectionsTo(node)
+  {
+    return this.getConnectionsOf(node).filter((connection) => connection.to === node);
   }
 
   createListDataView(amount, filterFunction, sortFunction)
