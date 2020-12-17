DeutscheSoft/aux-widgets

commit 203a7fbbbce8e5191faba7a5c598658458eeaf53

Documentation for matrix widgets

src/matrix/widgets/virtualtree.js
@@ -139,6 +139,27 @@ function subscribeAll() {
   );
 }
 
+/**
+ * VirtualTree is a scrollable list of {@link VirtualTreeEntry}s. It
+ * relies on a data model {@link VirtualTreeDataView}. Its purpose is
+ * to reduce the amount of elements in DOM by only holding the entries
+ * which fit inside the area. On scrolling the entries are re-sorted,
+ * re-subscribed and micro-scrolled to give the impression of seamless
+ * scrolling. It is used to display collapsable lists of hundrets or
+ * even thousands of entries in a CPU-friendly way.
+ *
+ * @param {Object} [options={ }] - An object containing initial options.
+ *
+ * @property {Integer} [options.size=32] - The size of a single entry in
+ *   the list.
+ * @property {Object} [options.entry_class=VirtualTreeEntry] - The class
+ *   to derive new entries from.
+ * @property {Object} options.virtualtreeview - The {@link VirtualTreeDataView}.
+ *
+ * @extends Container
+ *
+ * @class VirtualTree
+ */
 export const VirtualTree = defineClass({
   Extends: Container,
   _options: Object.assign(Object.create(Container.prototype._options), {
@@ -165,7 +186,7 @@ export const VirtualTree = defineClass({
     set_virtualtreeview: function (virtualtreeview) {
       this.virtualtreeview_subs.unsubscribe();
     },
-    scrollTopChanged: function (position) {
+    scrolltopchanged: function (position) {
       const O = this.options;
       const startIndex = Math.floor(position / O.size);
 
@@ -181,9 +202,23 @@ export const VirtualTree = defineClass({
       if (!this._scroll_event_suppressed) return;
 
       this._scroll_event_suppressed = false;
+      /**
+       * Is fired on scrolling the list.
+       *
+       * @event VirtualTree#scrollTopChanged
+       *
+       * @param {Integer} scroll - The amount of pixels scrolled from top.
+       */
       this.emit('scrollTopChanged', this._scrollbar.scrollTop);
     });
   },
+  /**
+   * Create and return a new entry based on `options.entry_class`.
+   *
+   * @method VirtualTree#createEntry
+   *
+   * @returns {Object} entry - The newly created entry instance.
+   */
   createEntry: function () {
     return new this.options.entry_class();
   },
@@ -203,6 +238,13 @@ export const VirtualTree = defineClass({
       this.set('virtualtreeview', options.virtualtreeview);
     this.triggerResize();
   },
+  /**
+   * Scroll the VirtualTree to this position.
+   *
+   * @param {Integer} position - The position to scroll to.
+   *
+   * @method VirtualTree#scrollTo
+   */
   scrollTo: function (position) {
     this._scroll_timer.restart(100);
     this.update('_scroll', position);
@@ -270,10 +312,17 @@ export const VirtualTree = defineClass({
     Container.prototype.resize.call(this);
   },
 });
-
+/**
+ * @member {HTMLDiv} VirtualTree#_scrollbar - A container for hiding
+ *   scrollbars. Has class <code>.aux-scrollbar</code>.
+ */
 defineChildElement(VirtualTree, 'scrollbar', {
   show: true,
 });
+/**
+ * @member {HTMLDiv} VirtualTree#_scroller - A container holding the
+ *   {@link VirtualTreeEntry}s. Has class <code>.aux-scroller</code>.
+ */
 defineChildElement(VirtualTree, 'scroller', {
   show: true,
   append: function () {

src/matrix/widgets/canvas.js
@@ -20,6 +20,16 @@
 import { defineClass, defineChildElement } from './../../widget_helpers.js';
 import { Widget } from './../../widgets/widget.js';
 
+/**
+ * Canvas is an SVG drawing connection paths in {@link Topology} and
+ * {@link Juxtaposition} patchpay widgets.
+ *
+ * @param {Object} [options={ }] - An object containing initial options.
+ *
+ * @extends Widget
+ *
+ * @class Canvas
+ */
 export const Canvas = defineClass({
   Extends: Widget,
   _options: Object.assign(Object.create(Widget.prototype._options), {}),

src/matrix/widgets/indicator.js
@@ -22,6 +22,26 @@ import { addClass, toggleClass } from './../../utils/dom.js';
 
 import { Button } from './../../widgets/button.js';
 
+/**
+ * Indicator is a button element inside the {@link Indicators} widget
+ * for the {@link Matrix}. All properties are reflected as class onto
+ * the element.
+ *
+ * @param {Object} [options={ }] - An object containing initial options.
+ *
+ * @property {Boolean} [options.connected] - Sink and source represented by this
+ *   indicator are connected.
+ * @property {Boolean} [options.connectable] - Sink and source represented by
+ *   this indicator can be connected, no restrictions.
+ * @property {Boolean} [options.sourceisgroup] - The source is a group header.
+ * @property {Boolean} [options.sinkisgroup] - The sink is a group header.
+ * @property {Boolean} [options.isgroup] - Either source or sink is a group header.
+ *
+ * @extends Button
+ *
+ * @class Indicator
+ */
+
 export const Indicator = defineClass({
   Extends: Button,
   _options: Object.assign(Object.create(Button.prototype._options), {
@@ -36,6 +56,18 @@ export const Indicator = defineClass({
     this.source = null;
     this.sink = null;
   },
+  /**
+   * Updates the Indicator with the relevant data.
+   *
+   * @method updateData
+   *
+   * @param {Integer} index1 - The index of the entry in the first list.
+   * @param {Integer} index2 - The index of the entry in the second list.
+   * @param {ConnectionData} connection - The connection data.
+   * @param {PortData} source - The source data.
+   * @param {PortData} sink - The sink data.
+   *
+   */
   updateData: function (index1, index2, connection, source, sink) {
     this.update('connected', !!connection);
     this.update('sourceisgroup', source && source.isGroup);

src/matrix/widgets/indicators.js
@@ -36,6 +36,27 @@ function onIndicatorClicked() {
   indicators.emit('indicatorClicked', this.source, this.sink);
 }
 
+/**
+ * Indicators is an area inside {@link Matrix} containing a matrix of
+ *   {@link Indicator}s displaying and managing connections.
+ *
+ * @param {Object}[options={ }] - An object containing initioal options.
+ *
+ * @property {Object} [options.indicator_class=Indicator] - the class to
+ *   derive new {@link Indicator}s from. Has to be a subclass of
+ *   {@link Indicator}.
+ * @property {Integer} [options.scroll_top=0] - The scroll position from
+ *   top.
+ * @property {Integer} [options.scroll_left=0] - The scroll position
+ *   from the left.
+ * @property {ConnectionView} options.connectionview - The
+ *   {@link ConnectionView} data model.
+ *
+ * @extends Container
+ *
+ * @class Indicators
+ */
+
 export const Indicators = defineClass({
   Extends: Container,
   _options: Object.assign(Object.create(Container.prototype._options), {
@@ -63,6 +84,14 @@ export const Indicators = defineClass({
 
       this._scroll_event_suppressed = false;
       const element = this.element;
+      /**
+       * Is fired on scrolling the area.
+       *
+       * @event Indicators#scrollChanged
+       *
+       * @param {Integer} scroll_top - The scroll position from top.
+       * @param {Integer} scroll_left - The scroll position from left.
+       */
       this.emit('scrollChanged', element.scrollTop, element.scrollLeft);
     });
     this.entries = [];
@@ -215,17 +244,35 @@ export const Indicators = defineClass({
       this.element.scrollTop = O.scroll_top;
     }
   },
-
+  /**
+   * Scroll the indicators area to this vertical (top) position.
+   *
+   * @param {Integer} position - the position in pixels to scroll to.
+   *
+   * @method Indicators#scrollTopTo
+   */
   scrollTopTo: function (position) {
     this.update('scroll_top', position);
     this._scroll_timer.restart(100);
   },
+  /**
+   * Scroll the indicators area to this horizontal (left) position.
+   *
+   * @param {Integer} position - the position in pixels to scroll to.
+   *
+   * @method Indicators#scrollLeftTo
+   */
   scrollLeftTo: function (position) {
     this.update('scroll_left', position);
     this._scroll_timer.restart(100);
   },
 });
 
+/**
+ * @member {HTMLDiv} Indicators#_scroller - The container for hiding
+ *   the scroll bar.
+ *   Has class <code>.aux-scroller</code>.
+ */
 defineChildElement(Indicators, 'scroller', {
   show: true,
 });

src/matrix/widgets/matrix.js
@@ -61,6 +61,25 @@ function setVirtualtreeviews() {
   }
 }
 
+/**
+ * A patchbay widget for handling connections between sources and sinks
+ * with matrix layout.
+ * It includes two {@link VirtualTree}s on top and left hand side and a
+ * {@link Indicators} widget displaying and handling the connections.
+ *
+ * @param {Object} [options={ }] - An object containing initial options.
+ *
+ * @property {Object} [options.indicator_class=Indicator] - The class to derive the
+ *   indicators inside the {@link Indicators} from.
+ * @property {String} [options.signal_flow='left-top'] - Define the direction of
+ *   the signal flow. Can be either `top-left` or `left-top`. This defines
+ *   the position of sinks and sources {@link VirtualTreeView} on the
+ *   screen.
+ *
+ * @extends Patchbay
+ *
+ * @class Matrix
+ */
 export const Matrix = defineClass({
   Extends: Patchbay,
   _options: Object.assign(Object.create(Patchbay.prototype._options), {
@@ -124,7 +143,10 @@ export const Matrix = defineClass({
     Patchbay.prototype.resize.call(this);
   },
 });
-
+/**
+ * @member {VirtualTree} Matrix#virtualtree_left - The {@link VirtualTree}
+ *   on the left hand side. Has class <code>.aux-virtualtreeleft</code>.
+ */
 defineChildWidget(Matrix, 'virtualtree_left', {
   create: VirtualTree,
   show: true,
@@ -135,7 +157,10 @@ defineChildWidget(Matrix, 'virtualtree_left', {
     class: 'aux-virtualtreeleft',
   },
 });
-
+/**
+ * @member {VirtualTree} Matrix#virtualtree_left - The {@link VirtualTree}
+ *   on top. Has class <code>.aux-virtualtreetop</code>.
+ */
 defineChildWidget(Matrix, 'virtualtree_top', {
   create: VirtualTree,
   show: true,
@@ -146,7 +171,10 @@ defineChildWidget(Matrix, 'virtualtree_top', {
     class: 'aux-virtualtreetop',
   },
 });
-
+/**
+ * @member {Indicators} Matrix#indicators - The {@link Indicators}
+ *   widget. Has class <code>.aux-indicators</code>.
+ */
 defineChildWidget(Matrix, 'indicators', {
   create: Indicators,
   show: true,

src/matrix/widgets/patchbay.js
@@ -28,6 +28,24 @@ import {
 
 import { Container } from './../../widgets/container.js';
 
+/**
+ * Patchbay is a generic widget for managing connections between sources
+ * and sinks. It relies on various data models like
+ * two {@link VirtualTreeDataView} and a {@link ConnectionDataView}.
+ *
+ * @param {Object} [options={ }] - An object containing initial options.
+ *
+ * @property {Object} options.sources - A {@link VirtualTreeDataView} containing
+ *   sources data.
+ * @property {Object} options.sinks - A {@link VirtualTreeDataView} containing
+ *   sinks data.
+ * @porperty {Integer} [options.size=32] - The size of a single entry in the
+ *   {@link VirtualTree}s.
+ *
+ * @extends Container
+ *
+ * @class Patchbay
+ */
 export const Patchbay = defineClass({
   Extends: Container,
   _options: Object.assign(Object.create(Container.prototype._options), {

src/matrix/widgets/virtualtreeentry.js
@@ -56,6 +56,42 @@ function composeDepth(tree_position) {
   return depth;
 }
 
+/**
+ * VirtualTreeEntry is the base class used in {@link VirtualTree}. It
+ * consists of a {@link Label}, an {@link Icon}, a {@link Button} for
+ * collapsing grouped entries and a {@link Label} displaying indentation
+ * icons. Overload this class if you want to add more items.
+ *
+ * @param {Object} [options={ }] - An onject containing initial options.
+ *
+ * @property {String} [options.label=false] - Set to a string to display a label,
+ *   set to `false` to entirely remove it from the DOM.
+ * @property {String} [options.icon=false] - Set to a string to display an icon,
+ *   set to `false` to entirely remove it from the DOM.
+ * @property {Array|Boolean} [options.depth=false] - An array containing
+ *   strings defining the indentation icons. Possible strings are
+ *   `trunk`, `branch`, `end` and `none`. Set to `false` to remove the
+ *   element from the DOM.
+ * @property {Boolean} [options.collapsable=false] - Defines if the entry
+ *   is collapsable. Collapsable items are displayed as group headers
+ *   with a button to toggle collapsed state.
+ * @property {Boolean}[options.collapsed=false] - Defines the collapsed
+ *   state of a group header and its descendants.
+ * @property {String} [options.icon_collapsed='arrowup'] - The icon to
+ *   display in groups collapse button for collapsed state.
+ * @property {String} [options.icon_uncollapsed='arrowdown'] - The icon to
+ *   display in groups collapse button for uncollapsed (open) state.
+ * @property {Boolean} [options.odd=false] - Defines the divisibility by two
+ *   of the index of the entry in the list. This sets the class aux-odd
+ *   to style alternating entries.
+ * @property {Boolean} [options.group=false] - Define if this entry is a
+ *   group header.
+ *
+ * @extends Container
+ *
+ * @class VirtualTreeEntry
+ */
+
 export const VirtualTreeEntry = defineClass({
   Extends: Container,
   _options: Object.assign(Object.create(Container.prototype._options), {
@@ -92,7 +128,13 @@ export const VirtualTreeEntry = defineClass({
     Container.prototype.draw.call(this, options, element);
     element.classList.add('aux-virtualtreeentry');
   },
-  // overload this to subscribe to other properties
+  /**
+   * This function is called internally to subscribe to properties
+   * Overload in order to handle additional data being displayed
+   * on custom entry classes.
+   *
+   * @method VirtualTree#subscribeData
+   */
   subscribeData: function () {
     const subs = this.data_subscriptions;
     const element = this.data;
@@ -112,7 +154,19 @@ export const VirtualTreeEntry = defineClass({
       })
     );
   },
-  // overload this to subscribe to other properties
+  /**
+   * This function is called internally on scroll to update the entries'
+   * content. Overload in order to handle additional data being displayed
+   * on custom entry classes.
+   *
+   * @method VirtualTree#subscribeData
+   *
+   * @param {Object} virtualtreeview - The VirtualTreeViewData object.
+   * @param {Integer} index - the index of the entry inside the list.
+   * @param {Object} data - The data model holding the properties values.
+   * @param {Integer} treeposition - An array containing information
+   *   about the entries indentation inside the tree.
+   */
   updateData: function (virtualtreeview, index, element, treePosition) {
     this.data_subscriptions.unsubscribe();
 

commit 5aa0122229aacf2ba4e702aee0222b60b8ee1de2

Matrix: Fix scroll handling in lists
This issue was accidentally introduced in commit
203a7fbbbce8e5191faba7a5c598658458eeaf53 when a typo was introduced in
the event name.

src/matrix/widgets/virtualtree.js
@@ -181,7 +181,7 @@ export const VirtualTree = defineClass({
     set_virtualtreeview: function (virtualtreeview) {
       this.virtualtreeview_subs.unsubscribe();
     },
-    scrolltopchanged: function (position) {
+    scrollTopChanged: function (position) {
       const O = this.options;
       const startIndex = Math.floor(position / O.size);
 