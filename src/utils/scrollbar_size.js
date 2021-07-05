554c8f39e48d050b0a36991555e86b7eb3bfb066
Arne Goedeke <arne@deuso.de>
2021-07-05 18:13:24 +0200

    scrollbarSize(): Measure scrollbar after dom content has been loaded
    In situations where aux is bundled and inlined scrollbarSize may be
    called before document.body has been created.
    
    Fixes #256

--- src/utils/scrollbar_size.js
+++ src/utils/scrollbar_size.js
@@ -1,24 +1,31 @@
 /**
  * Evaluate size of scroll bars. The size is set as CSS variable
  * `--aux-scrollbar-size` on the `body` element.
- * @returns {integer} Size in pixel.
+ * @returns {void}
  * @function scrollbarSize
  */
-var _scrollbar_size;
+let scrollbarMeasured = false;
+
+function measureScrollbarSize() {
+  const div = document.createElement('div');
+  div.style.overflow = 'scroll';
+  div.style.position = 'fixed';
+  div.style.visibility = 'hidden';
+  document.body.appendChild(div);
+  const size = div.offsetWidth - div.clientWidth;
+  document.body.removeChild(div);
+  document.body.style.setProperty(
+    '--aux-scrollbar-size',
+    size + 'px'
+  );
+}
 
 export function scrollbarSize() {
-  if (typeof _scrollbar_size === 'undefined') {
-    var div = document.createElement('div');
-    div.style.overflow = 'scroll';
-    div.style.position = 'fixed';
-    div.style.visibility = 'hidden';
-    document.body.appendChild(div);
-    _scrollbar_size = div.offsetWidth - div.clientWidth;
-    document.body.removeChild(div);
-    document.body.style.setProperty(
-      '--aux-scrollbar-size',
-      _scrollbar_size + 'px'
-    );
-  }
-  return _scrollbar_size;
+  if (scrollbarMeasured) return;
+  scrollbarMeasured = true;
+
+  if (document.body)
+    measureScrollbarSize();
+  else
+    document.addEventListener('DOMContentLoaded', measureScrollbarSize);
 }
