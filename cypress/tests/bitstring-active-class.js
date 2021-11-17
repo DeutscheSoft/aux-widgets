c611f6aac5ca8e163cfb3c8451ff4582d60dd5d2
Arne Goedeke <arne@deuso.de>
2021-11-17 10:52:44 +0100

    [testsuite]: Refactor bitstring test

--- cypress/tests/bitstring-active-class.js
+++ cypress/tests/bitstring-active-class.js
@@ -1,18 +1,31 @@
-describe('button test', () => {
-  it('successfully loads', () => {
+function beActive(button) {
+  expect(button).to.have.class('aux-active');
+  expect(button[0].auxWidget.get('state')).to.eq(true);
+}
+
+function beInactive(button) {
+  expect(button).to.not.have.class('aux-active');
+  expect(button[0].auxWidget.get('state')).to.eq(false);
+}
+
+function deactivateButton(button) {
+  return button.setAuxOption('state', false);
+}
+
+describe('Bitstring', () => {
+  it('click interaction', () => {
     cy.visit('http://localhost:1234/tests/examples/Bitstring.html');
 
-    cy.get('aux-bitstring .aux-button').eq(0).then(($el, index, $list) => {
+    const buttons = cy.get('aux-bitstring .aux-button');
 
-      cy.get($el).click().should('have.class', 'aux-active');
+    buttons.each(($el) => {
+      let button = deactivateButton(cy.wrap($el));
+
+      button.click()
+            .should(beActive);
+
+      button.click()
+            .should(beInactive);
     });
-
-    cy.get('aux-bitstring .aux-button').eq(1).then(($el, index, $list) => {
-
-      cy.get($el).click().should('not.have.class', 'aux-active');
-
-
-    });
-
   });
 });
