8a27230c6f14b24f8f699b6a444fc9f91e6c1ea8
Matt Hardy <matt@deuso.de>
2021-11-17 21:06:03 +0100

    [testsuite]: Confirm button more complete test and real awml file

---
+++ cypress/tests/confirmButton.js
@@ -0,0 +1,27 @@
+describe('ConfirmButton', () => {
+  it('test event, class and text', () => {
+    cy.visit('http://localhost:1234/tests/ConfirmButton.html');
+
+    cy.get('aux-confirmbutton').then(($el) => {
+
+      const cb = cy.spy();
+
+      cy.get('.aux-icon').should('have.class', 'warning');
+
+      cy.wrap($el)
+        .onAuxEvent('click', cb)
+        .click()
+        .then(() => {
+          expect(cb).to.be.called;
+          cy.get('.aux-icon').should('have.class', 'questionmark');
+          cy.get('.aux-label').should('have.text', "Are you sure?");
+        })
+        .wait(2000)
+        .then(() => {
+          cy.get('.aux-icon').should('have.class', 'warning');
+        }); 
+
+    });
+
+  });
+});
