5841565454532e0e7b7e492d08d05eab21af6b44
Arne Goedeke <arne@deuso.de>
2021-11-29 15:24:54 +0100

    Widget: schedule transition enabling more efficiently
    Before, enabling transitions happened in each widget using an individual
    call to setTimeout. This is very inefficient when creating many widget.
    To work around this issue, we schedule batches of calls which are close
    to each other in time.

--- src/widgets/widget.js
+++ src/widgets/widget.js
@@ -38,6 +38,9 @@ import {
 import { typecheckFunction } from '../utils/typecheck.js';
 import { GlobalResize } from '../utils/global_resize.js';
 import { GlobalVisibilityChange } from '../utils/global_visibility_change.js';
+import { ProximityTimers } from '../utils/timers.js';
+
+const enableTimers = new ProximityTimers();
 
 /* jshint -W089 */
 function Invalid(options) {
@@ -521,16 +524,16 @@ export const Widget = defineClass({
     if (notransitions === void 0) {
       O.notransitions = true;
 
-      const time =
-        O.notransitions_duration - (performance.now() - this._creation_time);
+      const targetTime = this._creation_time + O.notransitions_duration;
+      const time = targetTime - performance.now();
 
       const do_enable = () => {
         if (this.isDestructed()) return;
         this.enableTransitions();
       };
 
-      if (time > 0) {
-        setTimeout(do_enable, time);
+      if (time > 20) {
+        enableTimers.scheduleAt(do_enable, targetTime);
       } else {
         S.addNext(do_enable);
       }
