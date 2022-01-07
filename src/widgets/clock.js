88ec049d14c91769016f1d2020aa53034d4fb253
Arne Goedeke <arne@deuso.de>
2022-01-07 12:32:34 +0100

    Clock: use new renderer

--- src/widgets/clock.js
+++ src/widgets/clock.js
@@ -23,140 +23,10 @@ import { setText, element, addClass, getStyle } from '../utils/dom.js';
 import { makeSVG } from '../utils/svg.js';
 import { FORMAT } from '../utils/sprintf.js';
 import { S } from '../dom_scheduler.js';
+import { defineRender, deferMeasure, deferRender } from '../renderer.js';
 
 const formatViewbox = FORMAT('0 0 %d %d');
 
-function drawTime(force) {
-  let tmp, drawn;
-  const O = this.options;
-  let t = O.time;
-  if (!(t instanceof Date) || isNaN(t)) t = new Date();
-
-  if ((tmp = t.getSeconds()) !== this.__sec || force) {
-    this.circulars.seconds.set('value', tmp);
-    this.__sec = tmp;
-  }
-  if ((tmp = t.getMinutes()) !== this.__min || force) {
-    this.circulars.minutes.set('value', tmp);
-    this.__min = tmp;
-  }
-  if ((tmp = t.getHours() % 12) !== this.__hour || force) {
-    this.circulars.hours.set('value', tmp);
-    this.__hour = tmp;
-  }
-
-  const args = [t, O.fps, O.months, O.days];
-  if ((tmp = O.label.apply(this, args)) !== this.__label || force) {
-    setText(this._label, tmp);
-    this.__label = tmp;
-    drawn = true;
-  }
-  if ((tmp = O.label_upper.apply(this, args)) !== this.__upper || force) {
-    setText(this._label_upper, tmp);
-    this.__upper = tmp;
-    drawn = true;
-  }
-  if ((tmp = O.label_lower.apply(this, args)) !== this.__lower || force) {
-    setText(this._label_lower, tmp);
-    this.__lower = tmp;
-    drawn = true;
-  }
-
-  if (drawn)
-    /**
-     * Is fired when the time was drawn.
-     *
-     * @param {Date} time - The time which was drawn.
-     *
-     * @event Clock#timedrawn
-     */
-    this.emit('timedrawn', O.time);
-}
-function setLabels() {
-  const O = this.options;
-  const E = this._label;
-  const s = O.label(
-    new Date(2000, 8, 30, 24, 59, 59, 999),
-    2000,
-    8,
-    30,
-    6,
-    24,
-    59,
-    59,
-    999,
-    999,
-    O.months,
-    O.days
-  );
-  setText(E, s);
-
-  E.setAttribute('transform', '');
-
-  /* FORCE_RELAYOUT */
-
-  S.add(
-    function () {
-      let bb = E.getBoundingClientRect();
-      const eb = this.svg.getBoundingClientRect();
-      const size = Math.min(eb.width, eb.height);
-      if (bb.width === 0) return; // we are hidden
-      const mleft = parseInt(getStyle(E, 'margin-left')) || 0;
-      const mright = parseInt(getStyle(E, 'margin-right')) || 0;
-      const mlabel = (O.label_margin / 100) * size;
-      const space = size - mleft - mright - this._margin * 2 - mlabel * 2;
-      const scale = space / bb.width;
-      const pos = O.size / 2;
-
-      S.add(
-        function () {
-          E.setAttribute(
-            'transform',
-            'translate(' + pos + ',' + pos + ') ' + 'scale(' + scale + ')'
-          );
-
-          /* FORCE_RELAYOUT */
-
-          S.add(
-            function () {
-              bb = E.getBoundingClientRect();
-
-              S.add(
-                function () {
-                  this._label_upper.setAttribute(
-                    'transform',
-                    'translate(' +
-                      pos +
-                      ',' +
-                      O.label_upper_pos * O.size +
-                      ') ' +
-                      'scale(' +
-                      scale * O.label_scale +
-                      ')'
-                  );
-                  this._label_lower.setAttribute(
-                    'transform',
-                    'translate(' +
-                      pos +
-                      ',' +
-                      O.label_lower_pos * O.size +
-                      ') ' +
-                      'scale(' +
-                      scale * O.label_scale +
-                      ')'
-                  );
-                  drawTime.call(this, true);
-                }.bind(this),
-                1
-              );
-            }.bind(this)
-          );
-        }.bind(this),
-        1
-      );
-    }.bind(this)
-  );
-}
 function timeout() {
   if (this.__to) window.clearTimeout(this.__to);
   const O = this.options;
@@ -171,6 +41,7 @@ function timeout() {
 
     d.setTime(ts);
     this.set('time', d);
+    this.invalidate('time');
 
     let targ = O.timeout | 0;
     if (O.timeadd) {
@@ -320,13 +191,154 @@ export class Clock extends Widget {
     };
   }
 
+  static get renderers() {
+    return [
+      defineRender('size', function (size) {
+        this.svg.setAttribute('viewBox', formatViewbox(size, size));
+      }),
+      defineRender(
+        [ 'thickness', 'margin', 'size', 'show_hours', 'show_minutes', 'show_seconds' ],
+        function (thickness, margin, size, show_hours, show_minutes, show_seconds) {
+          const show = {
+            hours: show_hours,
+            minutes: show_minutes,
+            seconds: show_seconds,
+          };
+
+          let _margin = 0;
+
+          for (const name in this.circulars) {
+            if (!Object.prototype.hasOwnProperty.call(this.circulars, name)) continue;
+            const circular = this.circulars[name];
+
+            if (show[name]) {
+              circular.set('thickness', thickness);
+              circular.set('show_base', true);
+              circular.set('show_value', true);
+              circular.set('size', size);
+              circular.set('margin', _margin);
+              _margin += thickness;
+              _margin += circular.getStroke();
+              _margin += margin;
+            } else {
+              circular.set('show_base', false);
+              circular.set('show_value', false);
+            }
+          }
+
+          this.set('_margin', _margin);
+        }),
+
+      defineRender(
+        [ 'label', 'months', 'days', 'label_margin', 'size', 'label_upper_pos', 'label_scale', 'label_lower_pos', '_margin' ],
+        function (label, months, days, label_margin, size, label_upper_pos, label_scale, label_lower_pos, _margin) {
+          const { _label } = this;
+
+          setText(
+            _label, 
+            label(
+              new Date(2000, 8, 30, 24, 59, 59, 999),
+              2000,
+              8,
+              30,
+              6,
+              24,
+              59,
+              59,
+              999,
+              999,
+              months,
+              days
+            ));
+          _label.setAttribute('transform', '');
+
+          return deferMeasure(() => {
+            let bb = _label.getBoundingClientRect();
+            const eb = this.svg.getBoundingClientRect();
+            const size = Math.min(eb.width, eb.height);
+            if (bb.width === 0) return; // we are hidden
+            const mleft = parseInt(getStyle(_label, 'margin-left')) || 0;
+            const mright = parseInt(getStyle(_label, 'margin-right')) || 0;
+            const mlabel = (label_margin / 100) * size;
+            const space = size - mleft - mright - _margin * 2 - mlabel * 2;
+            const scale = space / bb.width;
+            const pos = size / 2;
+
+
+            return deferRender(() => {
+              _label.setAttribute(
+                'transform',
+                'translate(' + pos + ',' + pos + ') ' + 'scale(' + scale + ')'
+              );
+
+              return deferMeasure(() => {
+                bb = _label.getBoundingClientRect();
+
+                return deferRender(() => {
+                  this._label_upper.setAttribute(
+                    'transform',
+                    'translate(' +
+                      pos +
+                      ',' +
+                      label_upper_pos * size +
+                      ') ' +
+                      'scale(' +
+                      scale * label_scale +
+                      ')'
+                  );
+                  this._label_lower.setAttribute(
+                    'transform',
+                    'translate(' +
+                      pos +
+                      ',' +
+                      label_lower_pos * size +
+                      ') ' +
+                      'scale(' +
+                      scale * label_scale +
+                      ')'
+                  );
+
+                  // draw the time
+                  this.invalidate('time');
+                });
+              });
+            });
+          });
+        }),
+      defineRender(
+        [ 'time', 'label', 'label_upper', 'label_lower', 'fps', 'months', 'days' ],
+        function (time, label, label_upper, label_lower, fps, months, days) {
+          const { hours, seconds, minutes } = this.circulars;
+
+          if (!(time instanceof Date) || isNaN(time)) time = new Date();
+
+          seconds.set('value', time.getSeconds());
+          minutes.set('value', time.getMinutes());
+          hours.set('value', time.getHours());
+
+          const args = [time, fps, months, days];
+
+          setText(this._label, label.apply(this, args));
+          setText(this._label_upper, label_upper.apply(this, args));
+          setText(this._label_lower, label_lower.apply(this, args));
+          /**
+           * Is fired when the time was drawn.
+           *
+           * @param {Date} time - The time which was drawn.
+           *
+           * @event Clock#timedrawn
+           */
+          this.emit('timedrawn', time);
+        }),
+    ];
+  }
+
   initialize(options) {
     let SVG;
     /**
      * @member {Object} Clock#circulars - An object holding all three Circular as members <code>seconds</code>, <code>minutes</code> and <code>hours</code>.
      */
     this.circulars = {};
-    this._margin = -1;
     if (!options.element) options.element = element('div');
     super.initialize(options);
     this.set('time', this.options.time);
@@ -401,64 +413,6 @@ export class Clock extends Widget {
     element.appendChild(this.svg);
 
     super.draw(O, element);
-  }
-
-  redraw() {
-    const I = this.invalid,
-      O = this.options;
-
-    super.redraw();
-
-    if (I.size) {
-      this.svg.setAttribute('viewBox', formatViewbox(O.size, O.size));
-    }
-
-    if (
-      I.validate(
-        'show_hours',
-        'show_minutes',
-        'show_seconds',
-        'thickness',
-        'margin'
-      ) ||
-      I.size
-    ) {
-      let margin = 0;
-      for (const i in this.circulars) {
-        if (!Object.prototype.hasOwnProperty.call(this.circulars, i)) continue;
-        const circ = this.circulars[i];
-        if (O['show_' + i]) {
-          circ.set('thickness', O.thickness);
-          circ.set('show_base', true);
-          circ.set('show_value', true);
-          circ.set('size', O.size);
-          circ.set('margin', margin);
-          margin += O.thickness;
-          margin += circ.getStroke();
-          margin += O.margin;
-        } else {
-          circ.set('show_base', false);
-          circ.set('show_value', false);
-        }
-      }
-      if (this._margin < 0) {
-        this._margin = margin;
-      } else {
-        this._margin = margin;
-      }
-      // force setLabels
-      I.label = true;
-    }
-
-    if (I.validate('label', 'months', 'days', 'size', 'label_scale')) {
-      setLabels.call(this);
-    }
-
-    if (
-      I.validate('time', 'label', 'label_upper', 'label_lower', 'label_scale')
-    ) {
-      drawTime.call(this, false);
-    }
   }
 
   destroy() {
