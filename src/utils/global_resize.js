b15b39f6620bc58011d807d9251f91bab809f002
Arne Goedeke <arne@deuso.de>
2022-01-18 20:15:41 +0100

    GlobalResize: Trigger after font loading is done
    Font loading being complete often changes the size of elements.

--- src/utils/global_resize.js
+++ src/utils/global_resize.js
@@ -4,9 +4,11 @@ import { subscribeDOMEvent } from '../utils/events.js';
 export const GlobalResize = new EventMultiplexer((cb) => {
   const sub1 = subscribeDOMEvent(window, 'resize', () => cb());
   const sub2 = subscribeDOMEvent(window, 'load', () => cb());
+  const sub3 = subscribeDOMEvent(document.fonts, 'loadingdone', () => cb());
 
   return () => {
     sub1();
     sub2();
+    sub3();
   };
 });
