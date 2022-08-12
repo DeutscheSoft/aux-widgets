DeutscheSoft/aux-widgets

commit e8d2754932db863768c5dcdad33acf3cb52d42ef

Buttons: focus-able, remove ability to focus children, fix adding children via DOM nodes

src/widgets/buttons.js
@@ -272,6 +272,7 @@ export class Buttons extends Container {
       multi_select: 0,
       deselect: false,
       role: 'listbox',
+      tabindex: 0,
     };
   }
 
@@ -378,6 +379,7 @@ export class Buttons extends Container {
         options.set('id', createID('aux-button-'));
       }
       options.set('role', 'option');
+      options.set('tabindex', false);
       return options;
     } else {
       if (typeof options === 'string') {
@@ -390,7 +392,16 @@ export class Buttons extends Container {
       if (!options.id)
         options.id = createID('aux-button-');
       options.role = 'option';
+      options.tabindex = false;
       return new this.options.button_class(options);
+    }
+  }
+
+  addChild(child) {
+    super.addChild(child);
+
+    if (child instanceof Button) {
+      this.addButton(child);
     }
   }
 

commit b5f49d792ae1e1ec7bd36c5afbf331a169f3916c

Buttons: Fix a testsuite regression
This issue was introduced in e8d2754932db863768c5dcdad33acf3cb52d42ef.

src/widgets/buttons.js
@@ -500,14 +500,6 @@ export class Buttons extends Container {
     }
   }
 
-  addChild(child) {
-    super.addChild(child);
-
-    if (child instanceof Button) {
-      this.addButton(child);
-    }
-  }
-
   /**
    * Adds a {@link Button} to Buttons.
    *